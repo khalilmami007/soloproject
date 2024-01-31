817d5ef23b8aa5335bec81b654616806257fa999
khalilmami007 <136359722+khalilmami007@users.noreply.github.com>
2024-01-31 23:18:51 +0100

    .

--- bookclub/client/src/App.js
+++ bookclub/client/src/App.js
@@ -3,6 +3,7 @@ import { Routes,Route } from 'react-router-dom';
 import Registration from './components/Registration';
 import Books from './components/Books';
 import Edit from './components/Edit'
+import Favorite from './components/Favorite';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path='/' element={<Registration/>} />
       <Route path='/books' element={<Books/>} />
       <Route path='/books/:BookId' element={<Edit/>} />
+      <Route path='/favorite/:BookId' element={<Favorite/>} />
 
 
       </Routes>
